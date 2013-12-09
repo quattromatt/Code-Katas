37c3b59994806feac8981808d16b5a092221edbc
Matt Davis <mdavis@daxko.com>
2013-12-09 08:40:17 -0600

    Added a random start words generator method so that each execution is different

--- TomSwiftUnderMilkWood/main.rb
+++ TomSwiftUnderMilkWood/main.rb
@@ -14,10 +14,18 @@ class Trigrams
 		# print_text_map(text_map)
 
 		generator = TextGenerator.new
-		output = generator.generate_text(text_map, 'It was', 200)
+		start_words = pick_start_words(text_map)
+		output = generator.generate_text(text_map, start_words, 200)
 
 		formatted_output = ('Trigamerated output = "' + output + '"')
 		puts formatted_output
+	end
+
+	def pick_start_words(text_map)
+
+		random_index = rand(text_map.length)
+		text_map.keys[random_index]
+
 	end
 
 	def print_text_map(text_map)
