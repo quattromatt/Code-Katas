3b709db160c70abe1d3b1b9e48b4e08fc31b8671
Matt Davis <mdavis@daxko.com>
2014-01-13 08:39:17 -0600

    Started the actual translations of Roman numerals, started with the thousands

--- RomanNumerals/number_translator.rb
+++ RomanNumerals/number_translator.rb
@@ -8,23 +8,62 @@ class NumberTranslator
 
 		str_number = number.to_s()
 		len = str_number.length
-		index = len - 1
-		factor = 1
 
-		while index >= 0 do
+		index = 0
+		factor = 10 ** (len - 1)
+
+		while index < len do
 			number_part = str_number[index].to_i() * factor
 
-			translated = translate_part(number_part) + translated
+			translated = translated + translate_part(number_part, factor)
 
-			factor = factor * 10
-			index = index - 1
+			factor = factor / 10
+			index = index + 1
 		end
 
 		puts translated
 	end
 
-	def translate_part(number)
-		'TODO'
+	def translate_part(number, factor)
+
+		if number == 0
+			return '' 
+		end
+
+		case factor
+		when 1000
+			translate_thousands(number)
+		when 100
+			translate_hundreds(number)
+		when 10
+			translate_tens(number)
+		when 1
+			translate_ones(number)
+		else
+			'ERROR'
+		end
+
+	end
+
+	def translate_thousands(number)
+		translated = ''
+		while number > 0
+			translated = translated + 'M'
+			number = number - 1000
+		end
+		translated
+	end
+
+	def translate_hundreds(number)
+		'H'
+	end
+
+	def translate_tens(number)
+		'Te'
+	end
+
+	def translate_ones(number)
+		'O'
 	end
 
 end
